7515850d1eed3c40c7dd1e9dd9bb8bb3313cb894
Allan Aleson <chrollo.lucilfer2017@gmail.com>
2026-03-18 09:13:27 -0300

    Projeto inicial com testes

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+]);
